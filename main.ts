a97883a19b82036cbcdf9e1ca47e087db5521a49
huangshixuan1 <98266496+huangshixuan1@users.noreply.github.com>
2023-07-18 16:53:58 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,11 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 4) {
         左轉()
     } else if (receivedNumber == 8) {
-        上升()
+        直走()
     } else if (receivedNumber == 6) {
         右轉()
     } else if (receivedNumber == 2) {
-        下降()
+        後退()
     } else if (receivedNumber == 7) {
         抬東西起來()
     } else if (receivedNumber == 9) {
@@ -31,17 +31,17 @@ function 左轉 () {
     )
     sensors.DDMmotor(
     AnalogPin.P15,
-    1,
+    0,
     AnalogPin.P16,
-    200
+    0
     )
 }
 function 右轉 () {
     sensors.DDMmotor(
     AnalogPin.P13,
-    1,
+    0,
     AnalogPin.P14,
-    200
+    0
     )
     sensors.DDMmotor(
     AnalogPin.P15,
@@ -54,16 +54,50 @@ function 放東西下去 () {
 	
 }
 function 後退 () {
-	
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    200
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    1,
+    AnalogPin.P16,
+    200
+    )
 }
 function 直走 () {
-	
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    200
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    200
+    )
 }
 function 上升 () {
     pins.servoWritePin(AnalogPin.P8, 0)
 }
 function 停下來 () {
-	
+    pins.servoWritePin(AnalogPin.P8, 90)
+    sensors.DDMmotor(
+    AnalogPin.P13,
+    0,
+    AnalogPin.P14,
+    0
+    )
+    sensors.DDMmotor(
+    AnalogPin.P15,
+    0,
+    AnalogPin.P16,
+    0
+    )
 }
 function 下降 () {
     pins.servoWritePin(AnalogPin.P8, 180)
